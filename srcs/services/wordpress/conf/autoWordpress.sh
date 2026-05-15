2f0708def489fea70517963f195dcd31a3c5cfe7
Abdelghani Amraouy <186086629+amroabd11@users.noreply.github.com>
2026-05-15 11:13:42 +0100

    still there errors

--- srcs/services/wordpress/conf/autoWordpress.sh
+++ srcs/services/wordpress/conf/autoWordpress.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-wp config create --dbname=mariadb --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
-wp core install --url=$domainName --title=$siteTitle --admin_user=$adminUser --admin_password=$adminPass --admin_email=$emailAdm --allow-root
-wp user create $otherUserName $otherUserEmail --role=$othersRole --user_pass=$othersPass --allow-root
+mkdir -p /run/php
+
+echo -e "\nlisten=0.0.0.0:9000\n" >> /etc/php/8.4/fpm/pool.d/www.conf
+
+wp config create --dbname="$MYDB" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost=mariadb:3306 --allow-root
+wp core install --url="$domainName" --title="$siteTitle" --admin_user="$adminUser" --admin_password="$adminPass" --admin_email="$emailAdm" --allow-root
+wp user create "$otherUserName" "$otherUserEmail" --role="$othersRole" --user_pass="$othersPass" --allow-root
 
 
-echo $'\nlisten = 9000\n' >> /etc/php/8.4/fpm/pool.d/www.conf
 exec php-fpm8.4 -F
